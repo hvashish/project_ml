c5e96cca6ae4d402000d112a6537758d4e3075f9
Hari Vashishtha <hari.vashishtha@nokia.com>
2024-03-24 21:07:20 +0530

    bigos route added

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import os
 import platform
 import logging
 import logging.config
-
+import secrets
+secret_key = secrets.token_hex(16)
+# print('secrete_key:-',secret_key)
 from flask import Flask
 
 
 app = Flask(__name__)
-
+app.secret_key = '82a3649f9d7059e5fa53a2248e031821'
 from .views import views as views_blueprint
